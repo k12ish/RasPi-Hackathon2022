3b8d33a8deaed5ebd347a73cf80da92f8b5f3935
k12ish <45272873+k12ish@users.noreply.github.com>
2022-02-26 14:53:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 """
 Run this file to activate the fridge guard
 """
+from camerasystem import finish_recording, start_recording
+import time
+
+while True:
+    if accelorometer:
+        start_recording()
+        time.sleep(60)
+        finish_recording()
+    time.sleep(5)
