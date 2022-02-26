d79f568b0441ad8243cff965be2b69247596a1d0
k12ish <45272873+k12ish@users.noreply.github.com>
2022-02-26 14:30:47 +0000

    Restructure camerasystem.py

--- camerasystem.py
+++ camerasystem.py
@@ -10,41 +10,45 @@ import time, os, fnmatch, shutil
 import random
 
 def is_light_on():
-    time.sleep(5)
     if random.randint(0, 5) < 3:
         return False
     else:
         return True
 
 
-t = time.localtime()
-timestamp = time.strftime('%b-%d-%Y_%H%M', t)
-docu = ("real-time documentary - " + timestamp + ".h264")
-
-picam2 = Picamera2()
-video_config = picam2.video_configuration()
+PICAM2 = Picamera2()
+video_config = PICAM2.video_configuration()
 video_config["transform"] = libcamera.Transform(hflip=1, vflip=1)
 video_config["controls"]["NoiseReductionMode"] = 0
-picam2.configure(video_config)
+PICAM2.configure(video_config)
 
-preview = NullPreview(picam2)
-encoder = H264Encoder(10000000)
+PREVIEW = NullPreview(PICAM2)
+ENCODER = H264Encoder(10000000)
 
-encoder.output = open(docu, 'wb')
-picam2.encoder = encoder
-picam2.start_encoder()
-picam2.start()
-time.sleep(2)
-#size = picam2.sensor_resolution
+CURRENT_FILENAME = ""
 
-time.sleep(2)
+def start_recording():
+    assert CURRENT_FNAME == ""
+    timestamp = time.strftime('%b-%d-%Y_%H%M', time.localtime())
+    fname = "Recordings/" + timestamp + ".h264"
+    ENCODER.output = open(fname, 'wb')
+    PICAM2.encoder = ENCODER
+    PICAM2.start_encoder()
+    PICAM2.start()
+    CURRENT_FNAME = fname
+    return fname
+
+
+def finish_recording():
+    assert CURRENT_FILENAME != ""
+    PICAM2.stop()
+    PICAM2.stop_encoder()
+    fname, CURRENT_FILENAME = CURRENT_FILENAME, ""
+    return fname
+        
 """
 light_on = True
 while light_on:
     time.sleep(2)
     light_on = is_light_on()
     """
-picam2.stop()
-picam2.stop_encoder()
-    
-    
