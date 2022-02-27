82ad325b01e5516398f967d105aec7349189e79c
k12ish <k4krish@gmail.com>
2022-02-27 09:30:46 +0000

    Added signal handler for SIGQUIT, <C-\>

--- main.py
+++ main.py
@@ -4,18 +4,37 @@ Run this file to activate the fridge guard
 from camerasystem import finish_recording, start_recording
 from test_gpio import is_stealing
 from postprocess import process_video
-import time
+
+import time, signal
+
+FILENAMES = []
 
 
-while True:
+class GracefulExit:
+    def __init__(self):
+        self.kill_now = False
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+        signal.signal(signal.SIGQUIT, self.exit_gracefully)
+
+    def __bool__(self):
+        return self.kill_now
+
+    def exit_gracefully(self, *args):
+        self.kill_now = True
+
+
+should_exit = GracefulExit()
+while not should_exit:
     if is_stealing():
-        start_recording()
+        filename = start_recording()
         time.sleep(5)
-        fname = finish_recording()
-        print("Analysing Footage...")
-        if process_video(fname):
-            print("Found potential thief taking from fridge!!!")
-        else:
-            print("No faces found")
-        
-            
+        finish_recording()
+        FILENAMES.append(filename)
+
+for filename in FILENAMES:
+    print("Processing", filename)
+    if process_video(filename):
+        print("Found potential thief taking from fridge!!!")
+    else:
+        print("No faces found")
